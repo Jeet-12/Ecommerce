4506b86d5bd51e7f6e65624f396175e2bce6ffbe
Jeet Dewangan <109508132+Jeet-12@users.noreply.github.com>
2025-07-27 10:12:04 +0530

    Update image-upload.jsx

--- client/src/components/admin-view/image-upload.jsx
+++ client/src/components/admin-view/image-upload.jsx
@@ -50,7 +50,7 @@ function ProductImageUpload({
     const data = new FormData();
     data.append("my_file", imageFile);
     const response = await axios.post(
-      "https://ecommerce-1-ogsk.onrender.com/api/admin/products/upload-image",
+      "https://ecommerce-backend-c0r9.onrender.com/api/admin/products/upload-image",
       data
     );
     console.log(response, "response");
