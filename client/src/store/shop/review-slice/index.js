7bb5ec1a4f330de6282203fe78450d17f94cc066
Jeet Dewangan <109508132+Jeet-12@users.noreply.github.com>
2025-07-27 10:32:59 +0530

    Update index.js

--- client/src/store/shop/review-slice/index.js
+++ client/src/store/shop/review-slice/index.js
@@ -10,7 +10,7 @@ export const addReview = createAsyncThunk(
   "/order/addReview",
   async (formdata) => {
     const response = await axios.post(
-      `https://ecommerce-1-ogsk.onrender.com/api/shop/review/add`,
+      `https://ecommerce-backend-c0r9.onrender.com/api/shop/review/add`,
       formdata
     );
 
@@ -20,7 +20,7 @@ export const addReview = createAsyncThunk(
 
 export const getReviews = createAsyncThunk("/order/getReviews", async (id) => {
   const response = await axios.get(
-    `https://ecommerce-1-ogsk.onrender.com/api/shop/review/${id}`
+    `https://ecommerce-backend-c0r9.onrender.com/api/shop/review/${id}`
   );
 
   return response.data;
